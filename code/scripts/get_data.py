328f86a84006ee4f18439280c772e922b53d701b
Bala-A87 <bala8702@gmail.com>
2023-04-15 12:09:21 +0530

    Modify get_data to reflect selection methods tried

--- code/scripts/get_data.py
+++ code/scripts/get_data.py
@@ -1,4 +1,7 @@
 import pandas as pd
+from typing import Tuple, Union, Callable
+import numpy as np
+from sklearn.feature_selection import VarianceThreshold, SelectKBest, f_regression
 
 dataset = pd.read_csv('../data/dataset.tsv', sep='\t')
 data = dataset.drop(columns=['age'], inplace=False)
@@ -7,5 +10,54 @@ labels = dataset['age']
 const_cols = data.columns[data.std() == 0.0]
 data_non_zero_var = data.drop(columns=const_cols, inplace=False)
 
-def get_clean_data():
-    return data_non_zero_var, labels
+def get_processed_data(
+    corr_thresh: float = 0.95,
+    minmax_scale: bool = False,
+    var_thresh: float = None,
+    k_to_select: Union[int, str] = 'all',
+    selection_metric: Callable = f_regression
+) -> Tuple[pd.DataFrame, pd.Series]: 
+    """
+    Returns processed data from the age prediction dataset after some feature elimination.
+
+    Args:
+        corr_thresh (float, optional): Maximum correlation between features to allow. Any feature with larger correlation in the upper triangle of the correlation matrix will be dropped. Set to None to keep all features.
+            Defaults to 0.95.
+        minmax_scale (bool, optional): Whether to scale the data so that each column has values between 0.0 and 1.0.
+            Defaults to False.
+        var_thresh (float, optional): The minimum required variance of a feature to keep (after optional scaling). Set to None to keep all features.
+            Defaults to None.
+        k_to_select (int or str, optional): The number of features to select, after the above processing. Set to 'all' to select all features.
+            Defaults to 'all'.
+        selection_metric (Callable, optional): The selection metric to use to choose the top `k_to_select` features. Ideally a function from sklearn.feature_selection.
+            Defaults to sklearn.feature_selection.f_regression.
+        
+    Returns:
+        (data_proc, labels) (Tuple[pd.DataFrame, pd.Series]): The data after processing and the true labels.
+    """
+    data_proc = data_non_zero_var.copy()
+
+    if corr_thresh is not None:
+        corr_matrix = data_proc.corr().abs()
+        upper = corr_matrix.where(np.triu(np.ones(corr_matrix.shape), k=1).astype(bool))
+        corr_cols = [column for column in upper.columns if any(upper[column] > corr_thresh)]
+        data_proc.drop(columns=corr_cols, inplace=True)
+
+    if minmax_scale:
+        data_min = data_proc.min()
+        data_max = data_proc.max()
+        data_proc = (data_proc - data_min)/(data_max - data_min)
+    
+    if var_thresh is not None:
+        thresh = VarianceThreshold(var_thresh)
+        data_signif_var = thresh.fit_transform(data_proc)
+        cols_signif_var = thresh.get_feature_names_out(data_proc.columns)
+        data_proc = pd.DataFrame(data=data_signif_var, columns=cols_signif_var)
+    
+    if k_to_select != 'all' and selection_metric is not None:
+        feature_selector = SelectKBest(score_func=selection_metric, k=k_to_select)
+        data_selected = feature_selector.fit_transform(data_proc, labels)
+        cols_selected = feature_selector.get_feature_names_out(data_proc.columns)
+        data_proc = pd.DataFrame(data=data_selected, columns=cols_selected)
+
+    return data_proc, labels
