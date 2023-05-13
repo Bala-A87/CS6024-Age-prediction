d0344842268ea7ad0d642ec9f1aba7f71f5cf148
Bala-A87 <bala8702@gmail.com>
2023-05-13 14:19:37 +0530

    Fix bug in load data, run voting ensemble

--- code/modeling_ensemble.py
+++ code/modeling_ensemble.py
@@ -1,40 +1,32 @@
 from argparse import ArgumentParser
-import pandas as pd
 from timeit import default_timer as timer
+from scripts.get_data import get_processed_data
 from scripts.models import VotingClassifier
-from sklearn.linear_model import LinearRegression, LogisticRegression, ElasticNet
+from sklearn.feature_selection import SelectKBest, f_regression
+from sklearn.linear_model import LinearRegression, LogisticRegression
 from sklearn.svm import SVC, SVR
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestRegressor, HistGradientBoostingRegressor, HistGradientBoostingClassifier
 from sklearn.model_selection import LeaveOneOut
-from sklearn.metrics import mean_absolute_error, median_absolute_error, r2_score 
+from sklearn.metrics import mean_absolute_error as mae, median_absolute_error as med, r2_score as r2
 import numpy as np
-import matplotlib.pyplot as plt
-from seaborn import regplot
+from warnings import catch_warnings, simplefilter
 
 parser = ArgumentParser()
-parser.add_argument('-e', '--estimator', type=str, choices=['voting', 'svm', 'linreg', 'rf', 'hgb', 'elastic'], default='voting', help='Estimator to use')
+parser.add_argument('-e', '--estimator', type=str, choices=['voting', 'svm', 'linreg'], default='voting', help='Estimator to use')
 parser.add_argument('-bs', '--bin_size', type=int, default=20, help='Bin size for voting classifier')
-parser.add_argument('-b', '--base', type=str, choices=['lda', 'logreg', 'svm', 'knn', 'tree', 'hgb'], default='lda', help='Base estimator to use with voting estimator')
+parser.add_argument('-b', '--base', type=str, choices=['lda', 'logreg', 'svm'], default='lda', help='Base estimator to use with voting estimator')
 parser.add_argument('-C', type=float, default=1., help='Value of C for SVC in the voting ensemble')
-parser.add_argument('-r', '--ratio', type=float, default=0.5, help='Value of L1/L2 ration for elasticnet')
 
 args = parser.parse_args()
 
-data, labels = pd.read_csv('../data/data_proc.csv'), pd.read_csv('../data/labels.csv')
+data, labels = get_processed_data(corr_thresh=0.9, minmax_scale=True, var_thresh=0.01)
+data_arr, labels_arr = np.array(data), np.array(labels)
+print('Data loaded')
 
 if args.base == 'logreg':
     base_estimator = LogisticRegression()
 elif args.base == 'svm':
     base_estimator = SVC(kernel='linear', C=args.C)
-elif args.base == 'knn':
-    base_estimator = KNeighborsClassifier()
-elif args.base == 'tree':
-    base_estimator = DecisionTreeClassifier()
-elif args.base == 'hgb':
-    base_estimator = HistGradientBoostingClassifier()
 else:
     base_estimator = LDA(solver='eigen', shrinkage='auto')
 
@@ -43,62 +35,55 @@ def get_model():
         model = SVR(kernel='linear')
     elif args.estimator == 'linreg':
         model = LinearRegression()
-    elif args.estimator == 'rf':
-        model = RandomForestRegressor(criterion='absolute_error', max_features=0.9)
-    elif args.estimator == 'hgb':
-        model = HistGradientBoostingRegressor(loss='absolute_error')
-    elif args.estimator == 'elastic':
-        model = ElasticNet(l1_ratio=args.ratio)
     else:
         model = VotingClassifier(args.bin_size, base_estimator)
     return model
 
-desc_name = f'voting_{args.base}' if args.estimator == 'voting' else args.estimator
-desc_name += f'_{args.bin_size}'
+desc_name = f'{args.estimator}_{args.base}' if args.estimator == 'voting' else args.estimator
 
-log_path = f'../logs/logs_ensemble_{args.base}.txt'
+log_path = f'../logs/{desc_name}.txt'
 
 def add_log(log: str, end: str = '\n'):
     with open(log_path, mode='a') as f:
         f.write(log + end)
 
 add_log('*'*100, '\n\n')
-add_log(desc_name)
+add_log(desc_name+str(args.C) if args.estimator == 'voting' and args.base == 'svm' else desc_name)
 
 splitter = LeaveOneOut()
 true_labels = []
 pred_labels = []
 train_time, eval_time = 0.0, 0.0
 
-for fold, (train_ind, val_ind) in enumerate(splitter.split(data)):
-    model = get_model()
-    train_timer = timer()
-    if args.estimator == 'voting':
-        model.train(data.iloc[train_ind], labels.iloc[train_ind])
-    else:
-        model.fit(data.iloc[train_ind], labels.iloc[train_ind])
-    train_time += timer() - train_timer
-    eval_timer = timer()
-    pred = model.predict(data.iloc[val_ind]).reshape(-1,)
-    eval_time += timer() - eval_timer
-    true_labels.append(labels.iloc[val_ind].values[0])
-    pred_labels.append(pred[0])
-    add_log(f'[Fold {fold+1}/{splitter.get_n_splits(data)}] ==> True label: {true_labels[-1]}, Prediction: {pred_labels[-1]}')
+with catch_warnings():
+    for fold, (train_ind, val_ind) in enumerate(splitter.split(data_arr)):
+        simplefilter('ignore')
+        model = get_model()
+        X_train = data_arr[train_ind]
+        X_val = data_arr[val_ind]
+        Y_train = labels_arr[train_ind]
+        Y_val = labels_arr[val_ind]
+        feat_sel = SelectKBest(score_func=f_regression, k=1000)
+        X_train_sel = feat_sel.fit_transform(X_train, Y_train)
+        X_val_sel = feat_sel.transform(X_val)
+        
+        start_time = timer()
+        model.fit(X_train_sel, Y_train)
+        train_time += timer() - start_time
 
-add_log(f'Average train time: {(train_time / splitter.get_n_splits(data)):.6f} s, average eval time: {(eval_time / splitter.get_n_splits(data)):.6f} s')
+        start_time = timer()
+        pred = model.predict(X_val_sel).reshape(-1,)
+        eval_time += timer() - start_time
 
-mean_error = mean_absolute_error(np.array(true_labels), np.array(pred_labels))
-median_error = median_absolute_error(np.array(true_labels), np.array(pred_labels))
-r2 = r2_score(np.array(true_labels), np.array(pred_labels))
+        true_labels.append(Y_val[0])
+        pred_labels.append(pred[0])
 
-add_log(f'Mean absolute error: {mean_error}')
-add_log(f'Median absolute error: {median_error}')
-add_log(f'R2 score: {r2}', end='\n\n')
+add_log(f'Average train time: {(train_time / splitter.get_n_splits(data_arr)):.6f} s, average eval time: {(eval_time / splitter.get_n_splits(data_arr)):.6f} s')
 
+mae_val = mae(np.array(true_labels), np.array(pred_labels))
+med_val = med(np.array(true_labels), np.array(pred_labels))
+r2_val = r2(np.array(true_labels), np.array(pred_labels))
 
-plt.figure(figsize=(6, 6))
-regplot(x=true_labels, y=pred_labels)
-plt.title(desc_name)
-plt.xlabel('True ages')
-plt.ylabel('Predictions')
-plt.savefig(f'../plots/bin_size_cv/{desc_name}.png')
+add_log(f'Mean absolute error: {mae_val}')
+add_log(f'Median absolute error: {med_val}')
+add_log(f'R2 score: {r2_val}', end='\n\n')
--- code/scripts/get_data.py
+++ code/scripts/get_data.py
@@ -3,6 +3,8 @@ from typing import Tuple, Union, Callable
 import numpy as np
 from sklearn.feature_selection import VarianceThreshold, SelectKBest, f_regression, RFE
 from sklearn.linear_model import LinearRegression
+from pathlib import Path
+import os
 
 dataset = pd.read_csv('../data/dataset.tsv', sep='\t')
 data = dataset.drop(columns=['age'], inplace=False)
@@ -18,7 +20,8 @@ def get_processed_data(
     minmax_scale: bool = False,
     var_thresh: float = None,
     k_to_select: Union[int, str] = 'all',
-    selection_metric: Union[Callable, str] = f_regression
+    selection_metric: Union[Callable, str] = f_regression,
+    cache_path: str = '../data/'
 ) -> Tuple[pd.DataFrame, pd.Series]: 
     """
     Returns processed data from the age prediction dataset after some feature elimination.
@@ -38,42 +41,55 @@ def get_processed_data(
             Defaults to 'all'.
         selection_metric (Callable or str, optional): The selection metric to use to choose the top `k_to_select` features. Ideally a function from sklearn.feature_selection usable in sklearn.feature_selection.SelectKBest. Pass 'recursive' if recursive feature elimination using linear regression is to be used.
             Defaults to sklearn.feature_selection.f_regression.
+        cache_path (str, optional): Directory to save processed data in for quicker access. Not cached if None is passed.
+            Defaults to '../data/'.
         
     Returns:
         (data_proc, labels) (Tuple[pd.DataFrame, pd.Series]): The data after processing and the true labels.
     """
-    data_proc = data_non_zero_var.copy()
+    if Path(cache_path+'data_clean.tsv').is_file() and Path(cache_path+'labels.tsv').is_file():
+        data_proc = pd.read_csv(cache_path+'data_clean.tsv', sep='\t')
+        labels_proc = pd.read_csv(cache_path+'labels.tsv', sep='\t')
+    else:
+        data_proc = data_non_zero_var.copy()
+        labels_proc = labels.copy()
 
-    if log_transform:
-        data_proc = np.log2(data_proc + stability_coef)
+        if log_transform:
+            data_proc = np.log2(data_proc + stability_coef)
 
-    if corr_thresh is not None:
-        corr_matrix = data_proc.corr().abs()
-        upper = corr_matrix.where(np.triu(np.ones(corr_matrix.shape), k=1).astype(bool))
-        corr_cols = [column for column in upper.columns if any(upper[column] > corr_thresh)]
-        data_proc.drop(columns=corr_cols, inplace=True)
+        if corr_thresh is not None:
+            corr_matrix = data_proc.corr().abs()
+            upper = corr_matrix.where(np.triu(np.ones(corr_matrix.shape), k=1).astype(bool))
+            corr_cols = [column for column in upper.columns if any(upper[column] > corr_thresh)]
+            data_proc.drop(columns=corr_cols, inplace=True)
 
-    if minmax_scale:
-        data_min = data_proc.min()
-        data_max = data_proc.max()
-        data_proc = (data_proc - data_min)/(data_max - data_min)
+        if minmax_scale:
+            data_min = data_proc.min()
+            data_max = data_proc.max()
+            data_proc = (data_proc - data_min)/(data_max - data_min)
+        
+        if var_thresh is not None:
+            thresh = VarianceThreshold(var_thresh)
+            data_signif_var = thresh.fit_transform(data_proc)
+            cols_signif_var = thresh.get_feature_names_out(data_proc.columns)
+            data_proc = pd.DataFrame(data=data_signif_var, columns=cols_signif_var)
+        
+        if k_to_select != 'all' and selection_metric is not None:
+            if selection_metric == 'recursive':
+                feature_selector = RFE(estimator=LinearRegression(), n_features_to_select=k_to_select, step=0.1)
+                data_selected = feature_selector.fit_transform(data_proc, labels_proc)
+                cols_selected = feature_selector.get_feature_names_out(data_proc.columns)
+                data_proc = pd.DataFrame(data=data_selected, columns=cols_selected)
+            else:
+                feature_selector = SelectKBest(score_func=selection_metric, k=k_to_select)
+                data_selected = feature_selector.fit_transform(data_proc, labels_proc)
+                cols_selected = feature_selector.get_feature_names_out(data_proc.columns)
+                data_proc = pd.DataFrame(data=data_selected, columns=cols_selected)
     
-    if var_thresh is not None:
-        thresh = VarianceThreshold(var_thresh)
-        data_signif_var = thresh.fit_transform(data_proc)
-        cols_signif_var = thresh.get_feature_names_out(data_proc.columns)
-        data_proc = pd.DataFrame(data=data_signif_var, columns=cols_signif_var)
-    
-    if k_to_select != 'all' and selection_metric is not None:
-        if selection_metric == 'recursive':
-            feature_selector = RFE(estimator=LinearRegression(), n_features_to_select=k_to_select, step=0.1)
-            data_selected = feature_selector.fit_transform(data_proc, labels)
-            cols_selected = feature_selector.get_feature_names_out(data_proc.columns)
-            data_proc = pd.DataFrame(data=data_selected, columns=cols_selected)
-        else:
-            feature_selector = SelectKBest(score_func=selection_metric, k=k_to_select)
-            data_selected = feature_selector.fit_transform(data_proc, labels)
-            cols_selected = feature_selector.get_feature_names_out(data_proc.columns)
-            data_proc = pd.DataFrame(data=data_selected, columns=cols_selected)
+    if cache_path is not None:
+        if not Path(cache_path).is_dir():
+            os.mkdir(cache_path)
+        data_proc.to_csv(cache_path+'data_clean.tsv', sep='\t', index=None)
+        labels_proc.to_csv(cache_path+'labels.tsv', sep='\t', index=None)
 
-    return data_proc, labels
+    return data_proc, labels_proc
